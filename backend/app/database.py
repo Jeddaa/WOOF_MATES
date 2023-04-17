d0ff77e2a63a9f3d4233665263bbabe61fa1aebb
UnfazedAy <ayomidesoniyi@gmail.com>
2023-04-17 14:33:10 +0100

    Updated and cleaned my code

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,13 +3,9 @@ Database module
 contains the database methods and necessary setups
 """
 
-import logging
 from app.config import settings
-from app.models import User, Dog
-from app import schemas
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
 
 
 engine = create_engine(settings.DATABASE_URL)
