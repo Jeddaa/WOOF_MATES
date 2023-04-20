47ce9de85e82259f9be8fe9fa25b3b0ab3e712d0
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-04-21 00:43:46 +0100

    Routing Prolile Path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Landingpage/Home";
 import HomeFeed from "./components/Dashboard/Homefeed"; 
+import Profile from "./components/Dashboard/Profile";
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/homefeed" element={<HomeFeed/>}/>
+        <Route exact path="/profile" element={<Profile/>} />
       </Routes>
     </Router>
   );
