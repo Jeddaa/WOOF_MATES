f70161fb24ca52c588aee235f703f231d34e2862
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-04-21 00:23:52 +0100

    Added Feed route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Landingpage/Home";
+import HomeFeed from "./components/Dashboard/Homefeed"; 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/homefeed" element={<HomeFeed/>}/>
       </Routes>
     </Router>
   );
