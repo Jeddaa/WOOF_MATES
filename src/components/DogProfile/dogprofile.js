48463d8a28f5631808951a710626515cc365d71f
Nwosu Victor <97852276+VicNW@users.noreply.github.com>
2023-04-26 22:47:58 +0400

    dog profile

---
+++ src/components/DogProfile/dogprofile.js
@@ -0,0 +1,17 @@
+import useRef from "react";
+
+import Navbar from "../Landingpage/navbar";
+import ImageUploader from "./imageUploader";
+
+export default function dogProfile() {
+  
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <div></div>
+      <ImageUploader className="image_input" /> 
+         
+      
+    </div>
+  );
+}
---
+++ src/components/DogProfile/imageUploader.js
@@ -0,0 +1,36 @@
+import { useRef } from 'react';
+
+function ImageUploader() {
+  const inputRef = useRef(null);
+
+  const handleFileSelect = (event) => {
+    // Get the selected files from the input field
+    const files = event.target.files;
+    // Do something with the files, e.g. upload to server or display a preview
+    console.log(files);
+  };
+
+  const handleButtonClick = () => {
+    // Click the hidden input field when the button is clicked
+    inputRef.current.click();
+  };
+
+  return (
+    <div >
+      {/* Hidden input field */}
+      <input
+        type="file"
+        multiple
+        accept="image/*"
+        style={{ display: 'none' }}
+        ref={inputRef}
+        onChange={handleFileSelect}
+      />
+      {/* Button to trigger the hidden input field */}
+      <button onClick={handleButtonClick} className="image_input">Add Images</button>
+      
+    </div>
+  );
+}
+
+export default ImageUploader;
