095bb8eab2aaf2d25cf4d547a6c71dc85fa6b37f
Godsfavour Farida Momoh <kimfarida@Faridas-MacBook-Pro.local>
2023-04-27 03:01:15 +0100

    Feat:Integrated Login Page successully

--- src/components/Account/Register.js
+++ src/components/Account/Register.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
-import Navbar from "../Landingpage/navbar";
-import Footer from '../Landingpage/footer'
-import './Account.css'
+import { Link } from 'react-router-dom';
+import Navbar from '../Landingpage/navbar';
+import Footer from '../Landingpage/footer';
+import './Account.css';
 
 const Register = () => {
-const [formValues, setFormValues] = useState({
-profile_image: '',
-firstName:'',
-lastName:'',
-email: '',
-password: '',
-});
+  const [formValues, setFormValues] = useState({
+    profile_image: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
 
-const handleChange = e => {
-setFormValues({ ...formValues, [e.target.name]: e.target.value });
-};
+  const handleChange = e => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
 
-const URL = 'https://woof-mates.onrender.com/auth/signup';
+  const URL = 'https://woof-mates.onrender.com/auth/signup';
+  
 
-const handleSubmit = async () => {
+  const handleSubmit = async e => {
+    e.preventDefault();
+
     try {
       const formData = new FormData();
       formData.append('profile_image', formValues.profile_image);
@@ -27,19 +31,20 @@ const handleSubmit = async () => {
       formData.append('lastName', formValues.lastName);
       formData.append('email', formValues.email);
       formData.append('password', formValues.password);
-  
+
       const response = await axios.post(URL, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+          'Content-Type': 'multipart/form-data',
+        },
       });
-  
+
       if (response.status === 201) {
         // Handle the successful response here
-        console.log('User created successfully!');
+        alert('User created successfully!');
+        window.location.href = '/login';
       } else {
         // Handle errors
-        console.error('Error creating user.');
+        alert('Error creating user.');
       }
     } catch (error) {
       // Handle network errors
@@ -47,53 +52,44 @@ const handleSubmit = async () => {
     }
   };
 
-const validateForm = () => {
-return formValues.email && formValues.password; // && formValues.confirmPassword;
-}
+  const validateForm = () => {
+    return formValues.email && formValues.password; // && formValues.confirmPassword;
+  };
 
-return (
-<div>
-    <Navbar/>
-    <div className='form-container'>
-        <form>
-            <label>
-                <h1>Register</h1>
-            </label>
-            <br />
-            <label>
-                Profile Image:
-            </label>
-            <input type="file" name="profile_image" alt="profileImage" value={formValues.profile_image} onChange={handleChange} />
-            <label>
-                First Name:
-            </label>
-            <input name="firstName" value={formValues.firstName} onChange={handleChange} />
-            <label>
-                Last name:
-            </label>
-            <input name="lastName" value={formValues.lastName} onChange={handleChange} />
-            {/* <label>
-                Username:
-            </label>
-            <input name="username" value={formValues.username} onChange={handleChange} /> */}
-           
-            <label>
-                Email:
-            </label>
-            <input name="email" value={formValues.email} onChange={handleChange} />
-            
-            <label>
-                Password:
-            </label>
-            <input type="password" name="password" value={formValues.password} onChange={handleChange} />
-            
-            <button disabled={!validateForm()} onClick={handleSubmit} >Submit</button>
-            <h4>Have an account? <a href="/login">Login Now!</a></h4>
+  return (
+    <div>
+      <Navbar />
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <h1>Register</h1>
+          </label>
+          <br />
+          <label>Profile Image:</label>
+          <input
+            type="file"
+            name="profile_image"
+            alt="profileImage"
+            value={formValues.profile_image}
+            onChange={handleChange}
+          />
+          <label>First Name:</label>
+          <input name="firstName" value={formValues.firstName} onChange={handleChange} />
+          <label>Last Name:</label>
+          <input name="lastName" value={formValues.lastName} onChange={handleChange} />
+          <label>Email:</label>
+          <input name="email" value={formValues.email} onChange={handleChange} />
+          <label>Password:</label>
+          <input type="password" name="password" value={formValues.password} onChange={handleChange} />
+          <button disabled={!validateForm()}>Submit</button>
+          <h4>
+            Have an account? <Link to="/login">Login Now!</Link>
+          </h4>
         </form>
+      </div>
+      <Footer />
     </div>
-    <Footer/>
-</div>
-)
-}
+  );
+};
 
-export default Register
+export default Register;
