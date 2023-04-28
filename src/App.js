bd7f9e9e903ee29081bdfe8e294f3c8879e3c9fe
KimFarida <farimomoh@gmail.com>
2023-04-28 03:43:52 +0100

    Code Mop-Up

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { AuthProvider } from './components/Account/AuthProvider';
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Landingpage/Home";
-import HomeFeed from "./components/Dashboard/proto/Homefeed"; 
 import Profile from "./components/Dashboard/Profile";
 import Register from "./components/Account/Register";
 import Login from "./components/Account/Login";
-import ViewMatches from "./components/Dashboard/proto/ViewMatches"; // import the ViewMatches component
+// import HomeFeed from "./components/Dashboard/proto/Homefeed"; 
+// import ViewMatches from "./components/Dashboard/proto/ViewMatches"; // import the ViewMatches component
 
 function App() {
   return (
@@ -14,11 +14,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route exact path="/homefeed" element={<HomeFeed/>} />
           <Route exact path="/profile" element={<Profile/>} />
           <Route exact path="/register" element={<Register/>} />
           <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/matches/:dogId" element={<ViewMatches/>} /> {/* add the route for the ViewMatches component */}
+          {/* <Route exact path="/homefeed" element={<HomeFeed/>} /> */}
+          {/* <Route exact path="/matches/:dogId" element={<ViewMatches/>} /> add the route for the ViewMatches component */}
         </Routes>
       </Router>
     </AuthProvider>
