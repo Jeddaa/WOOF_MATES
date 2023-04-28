55993db09d9ba959a43b693a7d98837198374cc7
KimFarida <farimomoh@gmail.com>
2023-04-28 01:55:56 +0100

    Feat: Renders Auth User Details

---
+++ src/components/Dashboard/UserDetails.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './UserDetails.css';
+
+const UserDetails = ({ user }) => {
+  return (
+    <div className="profile">
+      <div className="photo-item photo">
+        <img src={user.profile_picture} style={{ width: '200px', height: '200px' }} alt={user.firstName} />
+      </div>
+      <div className="info">
+        <h2>User Details</h2>
+        <p>Name: {user.firstName} {user.lastName}</p>
+        <p>Email: {user.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserDetails;
